bb12c66d40ddc3da412b381ebad517fe74386001
Junior-Oliveira <franciscoanto@gmail.com>
2023-07-12 00:09:04 -0300

    Instalação do vuetify

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -31,7 +31,7 @@ const logout = () => {
     <div>
         <Head :title="title" />
 
-        <Banner />
+      
 
         <div class="min-h-screen bg-gray-100">
             <nav class="bg-white border-b border-gray-100">
@@ -274,13 +274,6 @@ const logout = () => {
                     </div>
                 </div>
             </nav>
-
-            <!-- Page Heading -->
-            <header v-if="$slots.header" class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
 
             <!-- Page Content -->
             <main>
