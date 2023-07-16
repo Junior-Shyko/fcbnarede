02fd3df8ea3cc8e2e3572da34cc2d2590d83b67f
Junior-Oliveira <franciscoanto@gmail.com>
2023-07-15 23:32:04 -0300

    :construction: Trabalhando no layout da edição da conta

--- resources/js/Layouts/LayoutApp.vue
+++ resources/js/Layouts/LayoutApp.vue
@@ -1,13 +1,37 @@
 <script setup>
-
-
+import { Head, Link, router } from "@inertiajs/vue3";
+import NavBarAppVue from "@/Components/NavBarApp.vue";
+import SideLeft from "@/Components/FCB/System/SideLeft.vue";
 </script>
 
 <template>
-    <v-app-bar></v-app-bar>
-    <main>
-                <slot />
-            </main>
+    <div>
+    <Head :title="title" />
+    <div class="min-h-screen bg-gray-100">
+        <NavBarAppVue />
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <v-app id="">
+                <v-main class="bg-[#F3F4F6]">
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12" sm="3">
+                                <SideLeft />
+                            </v-col>
+                            <v-col cols="12" sm="9">
+                                <main>
+                    <slot />
+                </main>
+                            </v-col>
+                        </v-row>
+                        
+                    </v-container>
+                </v-main>
+            </v-app>
+        </div>
+    </div>
+    </div>
+
+        
 </template>
 
 <script>
