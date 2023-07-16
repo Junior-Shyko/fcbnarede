3765ddf334599b391079f4069ef8afc3ba51b84d
Junior-Oliveira <franciscoanto@gmail.com>
2023-07-16 19:28:57 -0300

    :hammer: Add Model,Controller,Request e migration

---
+++ app/Http/Controllers/UserMetaDataController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserMetaDataRequest;
+use App\Http\Requests\UpdateUserMetaDataRequest;
+use App\Models\UserMetaData;
+
+class UserMetaDataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserMetaDataRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserMetaData $userMetaData)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserMetaData $userMetaData)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserMetaDataRequest $request, UserMetaData $userMetaData)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserMetaData $userMetaData)
+    {
+        //
+    }
+}
