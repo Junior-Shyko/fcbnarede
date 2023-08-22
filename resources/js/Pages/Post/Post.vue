799bd8b6f60a6af7210113ac200cf4722919cef9
Junior-Shyko <franciscoanto@gmail.com>
2023-08-21 22:51:44 -0300

    Refatorado componente post

--- resources/js/Pages/Post/Post.vue
+++ resources/js/Pages/Post/Post.vue
@@ -1,15 +1,49 @@
 <script setup>
 import LayoutApp from '@/Layouts/LayoutApp.vue';
+import api from '@/Services/server';
+import { Link, router } from '@inertiajs/vue3';
+import { ref, reactive } from "vue";
+import UsersLikes from "@/Components/FCB/Post/UsersLikes.vue"
+import moment from 'moment';
+import 'moment/dist/locale/pt-br'
+
 const props = defineProps({
-    post: Object,
-    users: Object
+  post: Object,
+  users: Object
 });
+
+const usersLikes = ref([])
+
+const userLikes = () => {
+  api.get('api/like/all/post/' + props.post.id)
+  .then(res => {
+    console.log({res})
+    usersLikes.value = res.data
+  })
+  .catch( err => {
+    console.log({err})
+  })
+}
+
+userLikes();
+
 </script>
 
 <template lang="">
    <LayoutApp title="Post">
    <v-row>
-    {{ users }}
+   <v-col cols="12">
+    <v-sheet
+      elevation="2"
+      class="mx-auto"
+      rounded="rounded"
+    >
+      <v-btn :href="route('dashboard')">
+        <v-icon icon="fas fa-arrow-left" class="mb-1"></v-icon> Voltar
+
+      </v-btn>
+    </v-sheet>
+   </v-col>
     <v-col cols="12" xs="9" sm="9">
     <v-card
     class="mx-auto"
@@ -24,7 +58,7 @@ const props = defineProps({
     </v-img>
 
     <v-card-subtitle class="pt-4">
-      Postado em: {{post.created_at}}
+      Postado à {{ moment([props.post.created_at]).toNow(true)}}
     </v-card-subtitle>
 
     <v-card-text>
@@ -34,87 +68,34 @@ const props = defineProps({
     <v-card-actions>
       <v-row>
         <v-col cols="12" xs="12">
-            <v-btn variant="tonal"
-         
-            >
+          <v-btn variant="tonal">
             Compartilhar
-            <v-icon
-            icon="fas fa-share-alt" class="mb-1"></v-icon>
-            </v-btn>
+            <v-icon icon="fas fa-share-alt" class="mb-1 ml-2"></v-icon>
+          </v-btn>
         </v-col>
-        <v-col cols="12"  class="d-flex justify-between" >
-            <v-list density="compact">
-      <v-list-subheader>
-        Curtidas
-        <v-icon icon="far fa-thumbs-up"
-        class="mb-2" color="info">
-        </v-icon>
-    
 
-      </v-list-subheader>
-      <v-list-item
-        value="Curtidas"
-        color="primary"
-      >
-        <template v-slot:prepend>
-          <v-avatar image="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-avatar>
-        </template>
+        <v-col cols="12" xs="12" sm=12 md="6">
+          <v-list density="compact">
+            <UsersLikes 
+              :usersLikes="usersLikes"
+              titleReaction="Curtidas"
+              iconReaction="fa-thumbs-up"
+              colorReaction="info" 
+              :userAuth="$page.props.auth.user.id" />     
+          </v-list>
+        </v-col>
 
-        <v-list-item-title v-text="`Paulo Sergio`"></v-list-item-title>
-      </v-list-item>
-    </v-list>
-
-    <v-list density="compact">
-      <v-list-subheader>Amou
-        <v-icon icon="far fa-heart"
-        class="mb-1" color="error">
-        </v-icon>
-      </v-list-subheader>
-      <v-list-item
-        value="Curtidas"
-        color="primary"
-      >
-        <template v-slot:prepend>
-     
-            <v-avatar image="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-avatar>
+        <v-col cols="12" xs="12" sm=12 md="6">
+          <v-list density="compact">
+            <UsersLikes
+              :usersLikes="usersLikes"
+              titleReaction="Amou"
+              iconReaction="fa-heart"
+              colorReaction="error" 
+              :userAuth="$page.props.auth.user.id"/>     
+          </v-list>
+        </v-col>
         
-            <v-list-item-title v-text="`Ana Paula da Silva`"></v-list-item-title>
-     
-        </template>
-        
-      </v-list-item>
-      <v-list-item
-        value="Curtidas"
-        color="primary"
-      >
-        <template v-slot:prepend>
-     
-            <v-avatar image="https://cdn.vuetifyjs.com/images/lists/3.jpg"></v-avatar>
-            <v-list-item-title v-text="`Amou`"></v-list-item-title>
-     
-        </template>
-        
-      </v-list-item>
-      <v-list-item
-        value="Curtidas"
-        color="primary"
-      >
-        <template v-slot:prepend>
-     
-            <v-avatar image="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-avatar>
-            <v-list-item-title v-text="`Amou`"></v-list-item-title>
-     
-        </template>
-        
-      </v-list-item>
-    </v-list>
-            
-
-    
-
-    </v-col>
-
-       
       </v-row>
       
     </v-card-actions>
@@ -124,7 +105,7 @@ const props = defineProps({
     <v-sheet
             elevation="4"
             height="200"
-            width="100%"          
+            width="100%"
           ></v-sheet>
     </v-col>
    </v-row>
