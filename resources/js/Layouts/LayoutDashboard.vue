2f2be9259d6834856741d89f9536df5719e5f95a
Junior-Shyko <franciscoanto@gmail.com>
2023-09-08 00:23:51 -0300

    Novo layout delete de usuario

---
+++ resources/js/Layouts/LayoutDashboard.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-app id="inspire">
+      <v-navigation-drawer v-model="drawer" color="blue-grey-lighten-4">
+        <v-card>
+            <v-sheet :height="65" >
+                <div class="d-flex justify-center pt-4">
+                    <h3 class="text-xl" >Boaz na rede</h3>
+                </div>
+            </v-sheet>
+            <v-layout>
+                
+            <v-navigation-drawer
+                permanent
+                color="blue-grey-lighten-4"
+            >
+                <v-list nav>
+               
+                  <v-list-item prepend-icon="fas fa-rss" title="Feed" value="inbox"
+                    color="primary"
+                    variant="plain"
+                    :to="route('dashboard')"
+                    >
+                  </v-list-item>
+               
+                <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
+                <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+            <v-main style="height: 250px"></v-main>
+            </v-layout>
+        </v-card>
+      </v-navigation-drawer>
+  
+      <v-app-bar>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>
+  
+      <v-main>
+        <!--  -->
+        <slot />
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script setup>
+    import { ref } from 'vue'
+    import { Link } from '@inertiajs/vue3';
+    const drawer = ref(null)
+  </script>
+  
+  <script>
+    export default {
+      data: () => ({ drawer: null }),
+    }
+  </script>
--- resources/js/Pages/User/List.vue
+++ resources/js/Pages/User/List.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, watch } from "vue";
 import api from "@/Services/server";
-
+import moment from "moment"
 import LayoutApp from '@/Layouts/LayoutApp.vue';
+import LayoutDashboard from "@/Layouts/LayoutDashboard.vue";
 
 const props = ref({
   
@@ -10,7 +11,7 @@ const props = ref({
 const state = reactive({
   posts: [],
   isLike: '',
-  itemsPerPage: 5,
+  itemsPerPage: 10,
   headers: [
     {
       title: 'Conhecido',
@@ -18,18 +19,28 @@ const state = reactive({
       sortable: false,
       key: 'nick',
     },
-    { title: 'CPF', align: 'end', key: 'cpf' },
-    { title: 'Dt. Niver', align: 'end', key: 'birth_date' },
-    { title: 'E-mail', align: 'end', key: 'email' },
-    { title: 'Nome Completo', align: 'end', key: 'name' },
+    { title: 'CPF', align: 'center', key: 'cpf' },
+    { title: 'Dt. Niver', align: 'center', key: 'birth_date' },
+    { title: 'E-mail', align: 'center', key: 'email' },
+    { title: 'Nome Completo', align: 'center', key: 'name' },
 
-    { title: 'Ação', key: 'actions', sortable: false, width: '16%' },
+    { title: 'Ação', key: 'actions', align: 'center', sortable: false, width: '16%' },
   ],
   desserts : [
    
   ],
   search: '',
+  dialogDelete : false
+  
 })
+
+const deleteItem = () => {
+  state.dialogDelete = true
+}
+
+const closedDelete = () => {
+  state.dialogDelete = false
+}
 
 const getUsers = () => {
   api.get('api/user/list')
@@ -46,7 +57,7 @@ const getUsers = () => {
 getUsers();
 </script>
 <template>
-  <LayoutApp title="Post">
+  <LayoutDashboard title="Post">
     <v-sheet class="d-flex align-center justify-center flex-wrap text-center mx-auto px-4" :elevation="2" width="100%"
       border rounded>
       <v-text-field
@@ -62,18 +73,37 @@ getUsers();
         :items="state.desserts"
         :search="state.search"
         item-value="name">
+        <template v-slot:item.birth_date="{ item }">
+          <label for="">{{ moment(item.birth_date).format("DD/MM/YYYY") }}</label>
+        </template>
         <template v-slot:item.actions="{ item }">
           <v-btn size="small" variant="text" @click="editItem(item.raw)">
             <v-icon icon="fas fa-edit" title="Editar Usuario"></v-icon>
           </v-btn>
-          <v-btn size="small" color="error" variant="text" @click="editItem(item.raw)">
+          <v-btn size="small" color="error" variant="text" @click="deleteItem(item.raw)">
             <v-icon icon="fas fa-trash" title="Excluir Usuario"></v-icon>
           </v-btn>
+
+          
         </template>
+        
       </v-data-table>
+      <v-dialog v-model="state.dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5 text-red-400" color="error">Deseja realmente fazer essa exclusão: </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <div  class="d-flex justify-space-between">
+                <v-btn color="blue-darken-1" variant="tonal"  @click="closedDelete" >Não</v-btn>
+                <v-btn color="error" variant="tonal">Sim</v-btn>
+              </div>
+                <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
     </v-sheet>
 
-  </LayoutApp>
+  </LayoutDashboard>
 </template>
 <script>
 export default {
